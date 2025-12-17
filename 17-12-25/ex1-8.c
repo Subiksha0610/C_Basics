d930cd8fef2ebf4520e34fad4f40e11e51192a90
Subiksha MV <mvsubiksha@gmail.com>
2025-12-17 21:07:56 +0530

    Add output comments to ex1-8.c

--- 17-12-25/ex1-8.c
+++ 17-12-25/ex1-8.c
@@ -13,3 +13,33 @@ else if(c=='\n')
 printf("No of Blanks: %d\n Number of Tabs: %d\n Number of NewLines: %d\n",
 cb,ct,cnl);
 }
+
+/* output 
+\t
+\n
+
+
+
+\n
+\n
+\n
+\t
+\t
+\t
+
+t
+y
+
+j
+m
+ m
+No of Blanks: 7
+ Number of Tabs: 0
+ Number of NewLines: 18
+
+
+
+No of Blanks: 0
+ Number of Tabs: 2
+ Number of NewLines: 2
+  */
