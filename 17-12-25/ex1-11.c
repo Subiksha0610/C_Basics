3379ec614c63ba66271cbb0ce18b9c3a7225d85b
Subiksha MV <mvsubiksha@gmail.com>
2025-12-17 21:13:30 +0530

    Add output examples for line, word, and character count

--- 17-12-25/ex1-11.c
+++ 17-12-25/ex1-11.c
@@ -22,3 +22,33 @@ int main() {
     printf("Number of line:%d\n Number of Words:%d\n Number of Character: %d\n", nl, nw, nc);
 return 0;
 }
+
+/* output
+Hello world\n
+Number of line:1
+ Number of Words:2
+ Number of Character: 14
+
+Hello world\tagain\n
+Number of line:1
+ Number of Words:2
+ Number of Character: 21
+
+//empty 
+Number of line:1
+ Number of Words:0
+ Number of Character: 1
+
+Number of line:0
+ Number of Words:0
+ Number of Character: 0
+
+Hello\nworld
+Number of line:1
+ Number of Words:1
+ Number of Character: 13
+
+Hello, world!\n
+Number of line:1
+ Number of Words:2
+ Number of Character: 16
