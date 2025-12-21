043b38a78b6a0fe6f70dcdc20394294573802be3
Subiksha MV <mvsubiksha@gmail.com>
2025-12-21 18:30:25 +0530

    Document ranges of variable types in ex2-1.c
    Added comments to explain the program's purpose and output.

--- 19-12-25/ex2-1.c
+++ 19-12-25/ex2-1.c
@@ -1,3 +1,8 @@
+/*Write a program to determine the ranges of char, short, int, and long 
+variables, both signed and unsigned, by printing appropriate values from standard headers 
+and by direct computation. Harder if you compute them: determine the ranges of the various 
+floating-point types. */
+
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
@@ -17,3 +22,14 @@ int main() {
 
     return 0;
 }
+
+/* output
+CHAR range: -128 to 127
+UNSIGNED CHAR range: 0 to 255
+INT range: -2147483648 to 2147483647
+UNSIGNED INT range: 0 to 4294967295
+LONG range: -9223372036854775808 to 9223372036854775807
+UNSIGNED LONG range: 0 to 18446744073709551615
+FLOAT range: 1.175494e-38 to 3.402823e+38
+DOUBLE range: 2.225074e-308 to 1.797693e+308
+*/
