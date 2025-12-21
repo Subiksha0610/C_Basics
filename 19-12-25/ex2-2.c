98bfdebc9944f92b2b7fbe667107775c70fba884
Subiksha MV <mvsubiksha@gmail.com>
2025-12-21 21:55:51 +0530

    Add output comments for fahr to celsius conversion
    Added output comments for temperature conversion.

--- 19-12-25/ex2-2.c
+++ 19-12-25/ex2-2.c
@@ -1,5 +1,5 @@
+//Write a loop equivalent to the for loop above without using && or ||. 
 #include <stdio.h>
-
 int main() {
     int fahr;
 
@@ -11,3 +11,22 @@ int main() {
 
     return 0;
 }
+
+/*output
+  0  -17.8
+ 20   -6.7
+ 40    4.4
+ 60   15.6
+ 80   26.7
+100   37.8
+120   48.9
+140   60.0
+160   71.1
+180   82.2
+200   93.3
+220  104.4
+240  115.6
+260  126.7
+280  137.8
+300  148.9
+*/
