d6747b78bea30c2e05d20c95297ff844ee1b64c4
Subiksha MV <mvsubiksha@gmail.com>
2025-12-22 10:45:04 +0530

    Update ex1-23.c

--- 19-12-25/ex1-23.c
+++ 19-12-25/ex1-23.c
@@ -1,44 +1,65 @@
 /*Write a program to remove all comments from a C program. Don't forget to 
 handle quoted strings and character constants properly. C comments don't nest.  */
 #include <stdio.h>
+
 int main() {
     int c, prev = 0;
     int state = 0;  // 0=normal, 1=comment, 2=string, 3=char
 
     while ((c = getchar()) != EOF) {
+
         if (state == 0) {
-            if (c == '/' && prev == '/') {
-                putchar(prev);
-                putchar(c);
+            if (prev == '/' && c == '/') {
+                while ((c = getchar()) != '\n' && c != EOF); 
                 prev = 0;
-            } else if (c == '*' && prev == '/') {
-                state = 1;
+                if (c == '\n')
+                    putchar('\n');
+                continue;
+            }
+            else if (prev == '/' && c == '*') {
+                state = 1; 
                 prev = 0;
                 continue;
-            } else {
-                if (c == '"') state = 2;
-                else if (c == '\'') state = 3;
+            }
+            else if (c == '"') {
                 if (prev) putchar(prev);
-                prev = c;
-            }
-        } else if (state == 1) {
-            if (prev == '*' && c == '/') {
-                state = 0;
+                putchar(c);
+                state = 2;
                 prev = 0;
                 continue;
             }
+            else if (c == '\'') {
+                if (prev) putchar(prev);
+                putchar(c);
+                state = 3;
+                prev = 0;
+                continue;
+            }
+            if (prev) putchar(prev);
             prev = c;
-        } else if (state == 2) {
+        }
+        else if (state == 1) {  
+            if (prev == '*' && c == '/') {
+                state = 0;  
+                prev = 0;
+            } else {
+                prev = c;  
+            }
+        }
+        else if (state == 2) {  
             putchar(c);
-            if (c == '"' && prev != '\\') state = 0;
+            if (c == '"' && prev != '\\')
+                state = 0;
             prev = c;
-        } else if (state == 3) {
+        }
+        else if (state == 3) {  
             putchar(c);
-            if (c == '\'' && prev != '\\') state = 0;
+            if (c == '\'' && prev != '\\')
+                state = 0;
             prev = c;
         }
     }
-    if (state == 0 && prev) putchar(prev);
+    if (state == 0 && prev) putchar(prev);  
     return 0;
 }
 
