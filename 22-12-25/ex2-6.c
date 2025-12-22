7d2c4011eac9fbdee684b1a313ff4c4ad282869b
Subiksha MV <mvsubiksha@gmail.com>
2025-12-22 20:58:20 +0530

    Implement setbits function to modify bits
    Added a function to set bits in an integer.

--- 22-12-25/ex2-6.c
+++ 22-12-25/ex2-6.c
@@ -1,3 +1,5 @@
+/*Write a function setbits(x,p,n,y) that returns x with the n bits that begin at 
+position p set to the rightmost n bits of y, leaving the other bits unchanged. */
 #include <stdio.h>
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
@@ -19,3 +21,11 @@ int main() {
     printf("Result: %u\n", result);
     return 0;
 }
+
+/*output
+Enter x: 200
+Enter p: 4
+Enter n: 3
+Enter y: 7
+Result: 220
+*/
