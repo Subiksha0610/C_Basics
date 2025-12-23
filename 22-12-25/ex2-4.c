2c175296c7f5e5bec8d8ac80874f495b410993a3
Subiksha MV <mvsubiksha@gmail.com>
2025-12-23 15:12:27 +0530

    Update ex2-4.c

--- 22-12-25/ex2-4.c
+++ 22-12-25/ex2-4.c
@@ -5,9 +5,9 @@ s1 that matches any character in the string s2.  */
 
 void squeeze(char s1[], char s2[]) {
     int i, j, k;
-    for (i = j = 0; s1[i] != '\0'; i++) {
-        for (k = 0; s2[k] != '\0' && tolower(s1[i]) != tolower(s2[k]); k++)
-            ;
+    j=0;
+    for (i = 0; s1[i] != '\0'; i++) {
+        for (k = 0; s2[k] != '\0' && tolower(s1[i]) != tolower(s2[k]); k++);
         if (s2[k] == '\0')
             s1[j++] = s1[i];
     }
