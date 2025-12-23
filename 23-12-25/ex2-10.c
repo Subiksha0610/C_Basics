768513e4c00e8df68cb32cf18fe4e2f64fe4747e
Subiksha MV <mvsubiksha@gmail.com>
2025-12-23 23:45:46 +0530

    Update ex2-10.c

--- 23-12-25/ex2-10.c
+++ 23-12-25/ex2-10.c
@@ -1,11 +1,48 @@
+/*Rewrite the function lower, which converts upper case letters to lower case, 
+with a conditional expression instead of if-else.  */
 #include <stdio.h>
 
 int lower(int c) {
-    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+    return (c >= 'A' && c <= 'Z') ? (c + ('a' - 'A')) : c;
 }
 
 int main() {
-    char c = 'C';
-    printf("%c\n", lower(c));
+    char c;
+
+    printf("Enter a character: ");
+    scanf(" %c", &c);  
+
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        printf("Output: %c\n", lower(c));
+    } else {
+        printf("Error: Enter a proper alphabet\n");
+    }
+
     return 0;
 }
+
+/*output
+Enter a character: A
+Output: a
+
+Enter a character: Z
+Output: z
+
+Enter a character: m
+Output: m
+
+Enter a character: 7
+Error: Enter a proper alphabet
+
+Enter a character: @
+Error: Enter a proper alphabet
+
+Enter a character:
+Error: Enter a proper alphabet
+
+Enter a character: [
+Error: Enter a proper alphabet
+
+Enter a character: \t
+Error: Enter a proper alphabet
+*/
