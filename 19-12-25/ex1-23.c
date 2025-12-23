caeaf44752c446f57aff9c71efde27b51aafe31f
Subiksha MV <mvsubiksha@gmail.com>
2025-12-23 09:44:55 +0530

    Update ex1-23.c

--- 19-12-25/ex1-23.c
+++ 19-12-25/ex1-23.c
@@ -3,66 +3,63 @@ handle quoted strings and character constants properly. C comments don't nest.  
 #include <stdio.h>
 
 int main() {
-    int c, prev = 0;
-    int state = 0;  // 0=normal, 1=comment, 2=string, 3=char
+    int c, next;
+    int state = 0; // 0=normal, 1=comment, 2=string, 3=char
 
     while ((c = getchar()) != EOF) {
-
         if (state == 0) {
-            if (prev == '/' && c == '/') {
-                while ((c = getchar()) != '\n' && c != EOF); 
-                prev = 0;
-                if (c == '\n')
-                    putchar('\n');
-                continue;
+
+            if (c == '/') {
+                next = getchar();
+                if (next == '/') {
+                    while ((c = getchar()) != '\n' && c != EOF);
+                    if (c == '\n') putchar('\n');
+                    continue;
+                } else if (next == '*') {
+                    state = 1;
+                    continue;
+                } else {
+                    putchar(c);
+                    c = next;
+                }
             }
-            else if (prev == '/' && c == '*') {
-                state = 1; 
-                prev = 0;
-                continue;
-            }
-            else if (c == '"') {
-                if (prev) putchar(prev);
-                putchar(c);
-                state = 2;
-                prev = 0;
-                continue;
-            }
-            else if (c == '\'') {
-                if (prev) putchar(prev);
-                putchar(c);
-                state = 3;
-                prev = 0;
-                continue;
-            }
-            if (prev) putchar(prev);
-            prev = c;
-        }
-        else if (state == 1) {  
-            if (prev == '*' && c == '/') {
-                state = 0;  
-                prev = 0;
-            } else {
-                prev = c;  
-            }
-        }
-        else if (state == 2) {  
+
+            if (c == '"') { putchar(c); state = 2; continue; }
+            if (c == '\'') { putchar(c); state = 3; continue; }
+
             putchar(c);
-            if (c == '"' && prev != '\\')
-                state = 0;
-            prev = c;
         }
-        else if (state == 3) {  
+
+
+        else if (state == 1) {
+            if (c == '*') {
+                next = getchar();
+                if (next == '/') { state = 0; continue; }
+                else c = next;
+            }
+ }
+
+
+        else if (state == 2) {
             putchar(c);
-            if (c == '\'' && prev != '\\')
-                state = 0;
-            prev = c;
+            if (c == '\\') {
+                c = getchar();
+                putchar(c);
+            } else if (c == '"') state = 0;
+        }
+
+
+        else if (state == 3) {
+            putchar(c);
+            if (c == '\\') {
+                c = getchar();
+                putchar(c);
+            } else if (c == '\'') state = 0;
         }
     }
-    if (state == 0 && prev) putchar(prev);  
+
     return 0;
 }
-
 
 //output
 //Hello /* remove this */ World
