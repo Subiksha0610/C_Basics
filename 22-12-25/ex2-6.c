eeb9fc364e30b06b7dae9f540b016965eaa35c52
Subiksha MV <mvsubiksha@gmail.com>
2025-12-23 09:01:20 +0530

    Update ex2-6.c

--- 22-12-25/ex2-6.c
+++ 22-12-25/ex2-6.c
@@ -3,7 +3,7 @@ position p set to the rightmost n bits of y, leaving the other bits unchanged. *
 #include <stdio.h>
 
 unsigned setbits(unsigned x, int p, int n, unsigned y) {
-    return (x & ~(~(~0 << n) << (p+1-n))) | ((y & ~(~0 << n)) << (p+1-n));
+    return (x & ((~0 << n) << (p+1-n))) | ((y & ~(~0 << n)) << (p+1-n));
 }
 
 int main() {
